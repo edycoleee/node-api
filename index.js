1c2dfa0d5ef0cffd8f9f102477a23f8142d91362
msb29 <yahyayahya.ymy@gmail.com>
2023-05-03 01:25:20 +0000

    error

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //index.js
 import express from "express";
 import ip from "ip";
-import PasienRouter from "./routes/PasienRoute.js";
+//import PasienRouter from "./routes/PasienRoute.js";
 import cors from "cors";
 import { logger } from "./middleware/logger.js";
 import errorHandler from "./middleware/errorHandler.js";
@@ -18,7 +18,7 @@ app.get('/', (req, res, next) => {
   res.send('Response tanpa Router')
 })
 
-app.use("/dtpasien", PasienRouter)
+//app.use("/dtpasien", PasienRouter)
 const PORT = 3500
 app.use(errorHandler)
 app.listen(PORT, () => console.log(`Server running on: ${ip.address()}:${PORT}`))
